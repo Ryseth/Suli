e5955c4a7c46f89ca94340faad516a25f25bff7d
Ryseth <rubbenke@yahoo.com>
2024-03-12 11:08:47 +0100

    Update 02_idegen_kulcsok.sql

--- SQL/02_idegen_kulcsok.sql
+++ SQL/02_idegen_kulcsok.sql
@@ -1,3 +1,9 @@
+
+ALTER table tantargyak ADD FOREIGN KEY (szakkor_azon) REFERENCES szakkor(azon);
+ALTER table tantargyak ADD FOREIGN KEY (tanterem_azon) REFERENCES tantermek(azon);
+ALTER table tantermek ADD FOREIGN KEY (osztaly_azon) REFERENCES osztalyok(azon);
+ALTER table szerzodeses_alkalmazottak ADD FOREIGN KEY (bankszamla_szam) REFERENCES bank(ugyfel_szamlaszam);
+
 ALTER table tanarok ADD FOREIGN KEY (bank_szamla_szam) REFERENCES bank(ugyfel_szamlaszam);
 
 ALTER table osztalyok  ADD FOREIGN KEY (osztalyfonok_azon) REFERENCES tanarok(azon);
@@ -30,5 +36,6 @@ ALTER table versenyzok ADD FOREIGN KEY (verseny_azon) REFERENCES versenyek(azon)
 
 ALTER table hetesek ADD FOREIGN KEY (diak_azon) REFERENCES diakok(azon);
 ALTER table hetesek ADD FOREIGN KEY (osztaly_azon) REFERENCES osztalyok(azon);
+
 alter table tranzakciok add foreign key (forras_szla_szam) references bank(ugyfel_szamlaszam);
 alter table tranzakciok add foreign key (cel_szla_szam) references bank(ugyfel_szamlaszam);
