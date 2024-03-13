4ac28a817221cec8b03d46370cb1b1efb09f3d92
Ryseth <rubbenke@yahoo.com>
2024-03-13 12:43:04 +0100

    Update 16_versenyek.sql

--- SQL/16_versenyek.sql
+++ SQL/16_versenyek.sql
@@ -1,1 +1,15 @@
 
+INSERT INTO suli.versenyek (datum,megnevezes,sport_azon) VALUES
+	 ('2021-08-24','Negyedik Megyei Verseny',29),
+	 ('2024-06-06','Negyedik Országos Verseny',16),
+	 ('2022-07-18','Nyolcadik Megyei Elődöntő',12),
+	 ('2023-02-19','Harmadik Országos Bajnokság',20),
+	 ('2023-10-14','Negyedik Megyei Elődöntő',9),
+	 ('2021-07-04','Hatodik Városi Verseny',26),
+	 ('2022-03-19','Nyolcadik Megyei Diákolimpia',6),
+	 ('2023-05-06','Harmadik Országos Diákolimpia',34),
+	 ('2022-02-02','Negyedik Országos Diákolimpia',33),
+	 ('2022-11-08','Harmadik Megyei Verseny',21);
+INSERT INTO suli.versenyek (datum,megnevezes,sport_azon) VALUES
+	 ('2021-11-02','Negyedik Megyei Verseny',2),
+	 ('2024-03-28','Hatodik Városi Verseny',32);
